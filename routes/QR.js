48d93cb819ed46c336ca9ce7a5520a239c6d0f8a
Asliddin Buriev <68139371+AsliddinBuriev@users.noreply.github.com>
2021-06-01 20:04:24 +0900

    style update

--- routes/QR.js
+++ routes/QR.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+
+
+
+
+
 router.post("/QR", (req, res) => {
     const phone = req.body.phone;
     const accountSid = 'AC99321e0fcd8ace4e5b3874c21a0013d1';
