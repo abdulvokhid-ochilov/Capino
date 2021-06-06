3eba6f8dbb99b95af40141d54f176beee20d7cc7
Asliddin Buriev <68139371+AsliddinBuriev@users.noreply.github.com>
2021-06-07 07:36:46 +0900

    update layouts with include

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require('path');
 const bodyParser = require("body-parser");
 const app = express();
 
 
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
